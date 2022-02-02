5a947c2a24927d402a18c273136335322d2386f6
Anders Cedronius <anders.cedronius@tickup.se>
2022-02-02 21:36:56 +0100

    first version send receive

--- multicast_receive.cpp
+++ multicast_receive.cpp
@@ -3,9 +3,41 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <kissnet.hpp>
 
-int main() {
-    std::cout << "Hello World!";
+const auto global_mcast_addr = "226.1.1.1"; //IPv4
+//const auto global_mcast_addr = "ff12::1234"; //IPv6
+const uint64_t global_mcast_port = 9000;
+
+const std::string global_message = "This is a multicast message payload";
+
+
+int main(int argc, char* argv[])
+{
+    kissnet::buffer<4000> receive_buffer;
+
+    std::cout << "kissnet multicast receive" << std::endl;
+
+    //Create a socket and join a multicast
+    auto mcast_listen_socket = kissnet::udp_socket();
+    mcast_listen_socket.join(kissnet::endpoint(global_mcast_addr, global_mcast_port));
+
+    //Get 100 packets
+    while (true) {
+        auto [received_bytes, status] = mcast_listen_socket.recv(receive_buffer);
+        if (!received_bytes || status != kissnet::socket_status::valid) {
+            std::cout << "Failed getting multicast data" << std::endl;
+        }
+        //Did we get the correct payload
+        uint64_t character_position = 0;
+        for(const char& c : global_message) {
+            if (c != (char)receive_buffer[character_position++]) {
+                std::cout << "Data missmatch " << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        std::cout << "Got multicast data of size " << received_bytes << std::endl;
+    }
     return 0;
 }
-
--- multicast_send.cpp
+++ multicast_send.cpp
@@ -1,10 +1,36 @@
 //
 // Created by Anders Cedronius on 2022-02-02.
 //
-
 #include <iostream>
+#include <thread>
+#include <kissnet.hpp>
 
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+int main(int argc, char* argv[])
+{
+    const auto mcast_addr = "226.1.1.1"; //IPv4
+    const uint64_t mcast_port = 9000;
+    const std::string mcast_message = "This is a multicast message payload";
+
+    std::cout << "kissnet multicast send packets started." << std::endl;
+    std::cout << "mcast IP -> " << mcast_addr << " mcast port -> " << mcast_port << std::endl;
+    //Max IPv6 UDP payload is 1452.. IPv4 is 1472
+    kissnet::buffer<1452> send_this;
+    std::memcpy(send_this.data(), mcast_message.c_str(), mcast_message.size());
+
+    //Tell kissnet where to send the multicast packets
+    kissnet::udp_socket mcast_send_socket(kissnet::endpoint(mcast_addr, mcast_port));
+    while (true) {
+        //Send the payload
+        auto[sent_bytes, status] = mcast_send_socket.send(send_this, sizeof(send_this));
+        if (sent_bytes != sizeof(send_this) || status != kissnet::socket_status::valid) {
+            std::cout << "kissnet multicast send failure" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        std::cout << "Sent multicast packet." << std::endl;
+
+        //Wait for 100 milliseconds
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    return EXIT_SUCCESS;
 }
