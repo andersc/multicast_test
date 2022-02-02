e99eab8422afe21646f2267169e3a5504f4bf4ff
Anders Cedronius <anders.cedronius@tickup.se>
2022-02-02 20:46:29 +0100

    Added base project

---
+++ multicast_receive.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Anders Cedronius on 2022-02-02.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+
---
+++ multicast_send.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Anders Cedronius on 2022-02-02.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
